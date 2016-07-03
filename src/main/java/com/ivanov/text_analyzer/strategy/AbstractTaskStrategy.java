741fefdec02529aae7e80585abf5103818f64727
HappyHappy1996 <semenrikirovi4@gmail.com>
2016-07-03 12:49:46 +0300

    changed regex

--- src/main/java/com/ivanov/text_analyzer/strategy/AbstractTaskStrategy.java
+++ src/main/java/com/ivanov/text_analyzer/strategy/AbstractTaskStrategy.java
@@ -5,7 +5,7 @@ package com.ivanov.text_analyzer.strategy;
  */
 public abstract class AbstractTaskStrategy implements TaskStrategy {
 
-	public static final String REGEX_SELECT_WORDS = "([a-zA-Zа-яА-Я]+)(, | \\(|\\) |\\. |[\\.,;:\\-' ])?";
+	public static final String REGEX_SELECT_WORDS = "([a-zA-Zа-яА-Я]+)(, | \\(|\\) |\\. |\\p{Punct})?";
 	
 	private String fileData;
 
